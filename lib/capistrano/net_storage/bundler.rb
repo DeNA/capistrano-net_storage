6d68a99c6a75caeaf08b3582f07ea6abe85dd0ce
Takumasa Ochi <aeroastro007@gmail.com>
2023-07-14 21:23:49 +0900

    Fix issue on local variable

--- lib/capistrano/net_storage/bundler.rb
+++ lib/capistrano/net_storage/bundler.rb
@@ -7,22 +7,24 @@ module Capistrano
       include Capistrano::NetStorage::Utils
 
       def check
+        c = config # should be local variable
         run_locally do
           execute :which, 'bundle'
-          execute :mkdir, '-p', config.local_bundle_path
+          execute :mkdir, '-p', c.local_bundle_path
         end
       end
 
       # bundle install locally. `.bundle/config` and installed gems are to be included in the release and archive.
       def install
+        c = config # should be local variable
         run_locally do
-          within config.local_release_app_path do
+          within c.local_release_app_path do
             ::Bundler.with_clean_env do
               install_path = Pathname.new('vendor/bundle') # must be a relative path for portability between local and remote
               execute :mkdir, '-p', install_path
 
               # Sync installed gems from shared directory to speed up installation
-              execute :rsync, '-a', '--delete', "#{config.local_bundle_path}/", install_path
+              execute :rsync, '-a', '--delete', "#{c.local_bundle_path}/", install_path
 
               # Always set config
               execute :bundle, 'config', 'set', '--local', 'deployment', 'true'
@@ -34,7 +36,7 @@ module Capistrano
               execute :bundle, 'clean'
 
               # Sync back to shared directory for the next release
-              execute :rsync, '-a', '--delete', "#{install_path}/", config.local_bundle_path
+              execute :rsync, '-a', '--delete', "#{install_path}/", c.local_bundle_path
             end
           end
         end
