7ae8e59d8e5a080109c31df44736e8f35c52ef7c
Takumasa Ochi <aeroastro007@gmail.com>
2023-07-14 17:44:45 +0900

    Remove caching paths

--- lib/capistrano/net_storage/config.rb
+++ lib/capistrano/net_storage/config.rb
@@ -22,8 +22,7 @@ module Capistrano
           raise ArgumentError, "Invalid type specified: #{type.inspect}"
         end
 
-        @executor_class ||= {}
-        @executor_class[type] ||= fetch(:"net_storage_#{type}") do
+        fetch(:"net_storage_#{type}") do
           default = DEFAULT_EXECUTOR_CLASS[type]
           unless default
             raise ArgumentError, "You have to `set(:net_storage_#{type}, CustomClass)`"
@@ -39,30 +38,26 @@ module Capistrano
       end
 
       def max_parallels
-        @max_parallels ||= fetch(:net_storage_max_parallels, servers.size)
+        fetch(:net_storage_max_parallels, servers.size)
       end
 
       # Application configuration files to be deployed with
       # @return [Array<String, Pathname>]
       def config_files
-        @config_files ||= fetch(:net_storage_config_files)
+        fetch(:net_storage_config_files)
       end
 
       # If +true+, skip to bundle gems bundled with target app.
       # Defaults to +true+
       def skip_bundle?
-        return @skip_bundle if instance_variable_defined? :@skip_bundle
-
-        @skip_bundle = !fetch(:net_storage_with_bundle, false) # just for backward compatibility
+        !fetch(:net_storage_with_bundle, false) # just for backward compatibility
       end
 
       # If +true+, create archive ONLY when it's not found on remote storage.
       # Otherwise, create archive ALWAYS.
       # Defaults to +true+
       def archive_on_missing?
-        return @archive_on_missing if instance_variable_defined? :@archive_on_missing
-
-        @archive_on_missing = fetch(:net_storage_archive_on_missing, true)
+        fetch(:net_storage_archive_on_missing, true)
       end
 
       # If +true+, use +rsync+ to sync config files.
@@ -70,21 +65,19 @@ module Capistrano
       # Defaults to +false+
       # @see #rsync_options
       def upload_files_by_rsync?
-        @upload_files_by_rsync ||= fetch(:net_storage_upload_files_by_rsync, false)
+        fetch(:net_storage_upload_files_by_rsync, false)
       end
 
       # You can set +:user+, +:keys+, +:port+ as ssh options for +rsync+ command to sync configs
       # when +:net_storage_upload_files_by_rsync+ is set +true+.
       # @see #upload_files_by_rsync?
       def rsync_options
-        @rsync_options ||= fetch(:net_storage_rsync_options, fetch(:ssh_options, {}))
+        fetch(:net_storage_rsync_options, fetch(:ssh_options, {}))
       end
 
       # If your repository consists of multiple Rails apps, you can enable this for seamless deployment
       def multi_app_mode?
-        return @multi_app_mode if instance_variable_defined? :@multi_app_mode
-
-        @multi_app_mode = fetch(:net_storage_multi_app_mode, false)
+        fetch(:net_storage_multi_app_mode, false)
       end
 
       #
@@ -93,72 +86,72 @@ module Capistrano
 
       # Path to application of remote release_path
       def release_app_path
-        @release_app_path ||= multi_app_mode? ? Pathname.new(release_path).join(fetch(:application)) : Pathname.new(release_path)
+        multi_app_mode? ? Pathname.new(release_path).join(fetch(:application)) : Pathname.new(release_path)
       end
 
       # Path of base directory on local
       # @return [Pathname]
       def local_base_path
-        @local_base_path ||= Pathname.new(fetch(:net_storage_local_base_path, "#{Dir.pwd}/.local_repo"))
+        Pathname.new(fetch(:net_storage_local_base_path, "#{Dir.pwd}/.local_repo"))
       end
 
       # Path to clone repository on local
       # @return [Pathname]
       def local_mirror_path
-        @local_mirror_path ||= Pathname.new(fetch(:net_storage_local_mirror_path, local_base_path.join('mirror')))
+        Pathname.new(fetch(:net_storage_local_mirror_path, local_base_path.join('mirror')))
       end
 
       # Path to keep release directories on local
       # @return [Pathname]
       def local_releases_path
-        @local_releases_path ||= Pathname.new(fetch(:net_storage_local_releases_path, local_base_path.join('releases')))
+        Pathname.new(fetch(:net_storage_local_releases_path, local_base_path.join('releases')))
       end
 
       # Path to take a snapshot of repository for release on local
       # @return [Pathname]
       def local_release_path
-        @local_release_path ||= local_releases_path.join(release_timestamp)
+        local_releases_path.join(release_timestamp)
       end
 
       # Path to application of local release_path
       def local_release_app_path
-        @local_release_app_path ||= multi_app_mode? ? local_release_path.join(fetch(:application)) : local_release_path
+        multi_app_mode? ? local_release_path.join(fetch(:application)) : local_release_path
       end
 
       # Shared directory to install gems on local
       # @return [Pathname]
       def local_bundle_path
-        @local_bundle_path ||= Pathname.new(fetch(:net_storage_local_bundle_path, local_base_path.join('bundle')))
+        Pathname.new(fetch(:net_storage_local_bundle_path, local_base_path.join('bundle')))
       end
 
       # Path of archive directories on local
       # @return [Pathname]
       def local_archives_path
-        @local_archives_path ||= Pathname.new(fetch(:net_storage_local_archives_path, local_base_path.join('archives')))
+        Pathname.new(fetch(:net_storage_local_archives_path, local_base_path.join('archives')))
       end
 
       # Destination path to archive application on local
       # @return [Pathname]
       def local_archive_path
-        @local_archive_path ||= local_archives_path.join("#{release_timestamp}.#{archive_file_extension}")
+        local_archives_path.join("#{release_timestamp}.#{archive_file_extension}")
       end
 
       # Path of archive directories on remote servers
       # @return [Pathname]
       def archives_path
-        @archives_path ||= Pathname.new(fetch(:net_storage_archives_path, deploy_path.join('net_storage_archives')))
+        Pathname.new(fetch(:net_storage_archives_path, deploy_path.join('net_storage_archives')))
       end
 
       # Path of archive file to be downloaded on remote servers
       # @return [Pathname]
       def archive_path
-        @archive_path ||= archives_path.join("#{release_timestamp}.#{archive_file_extension}")
+        archives_path.join("#{release_timestamp}.#{archive_file_extension}")
       end
 
       # Suffix of archive file
       # @return [String]
       def archive_file_extension
-        @archive_file_extension ||= executor_class(:archiver).file_extension
+        executor_class(:archiver).file_extension
       end
     end
   end
