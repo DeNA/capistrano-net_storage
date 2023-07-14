982ec242f7dc1669f9251a73197b11b8d2be924b
Takumasa Ochi <aeroastro007@gmail.com>
2023-07-14 19:10:00 +0900

    Refactor `bundle install` to reduce complexity

--- lib/capistrano/net_storage/bundler.rb
+++ lib/capistrano/net_storage/bundler.rb
@@ -15,25 +15,25 @@ module Capistrano
       # bundle install locally. `.bundle/config` and installed gems are to be included in the release and archive.
       def install
         run_locally do
-          local_release_bundle_path = config.local_release_path.join('vendor', 'bundle')
-          execute :mkdir, '-p', local_release_bundle_path
-
-          # Copy shared gems to release bundle path beforehand to reuse installed previously
-          execute :rsync, '-a', '--delete', "#{config.local_bundle_path}/", local_release_bundle_path
-
           within config.local_release_path do
             ::Bundler.with_clean_env do
+              install_path = Pathname.new('vendor/bundle')
+              execute :mkdir '-p', install_path
+
+              # Sync installed gems from shared directory to speed up installation
+              execute :rsync, '-a', '--delete', "#{config.local_bundle_path}/", install_path
+
               # Always set config
               execute :bundle, 'config', 'set', '--local', 'deployment', 'true'
-              execute :bundle, 'config', 'set', '--local', 'path', Pathname.new('vendor/bundle') # Use relative path to ease rsync
+              execute :bundle, 'config', 'set', '--local', 'path', install_path
               execute :bundle, 'config', 'set', '--local', 'without', 'development test'
               execute :bundle, 'config', 'set', '--local', 'disable_shared_gems', 'true'
 
               execute :bundle, 'install', '--quiet'
               execute :bundle, 'clean'
 
-              # Sync installed gems to shared directory to reuse them next time
-              execute :rsync, '-a', '--delete', "#{local_release_bundle_path}/", config.local_bundle_path
+              # Sync back to shared directory for the next release
+              execute :rsync, '-a', '--delete', "#{install_path}/", config.local_bundle_path
             end
           end
         end
