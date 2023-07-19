35c639ee28816af1eb7fa5c82462b039eaea22e5
Takumasa Ochi <aeroastro007@gmail.com>
2023-07-19 21:43:09 +0900

    Control max parallel and jitter separately for rsync

--- lib/capistrano/net_storage/utils.rb
+++ lib/capistrano/net_storage/utils.rb
@@ -4,6 +4,12 @@ module Capistrano
   module NetStorage
     # Common utility methods
     module Utils
+      # These values are intentionally separated from config and fixed.
+      # If you have trouble with these defaults for 10^2 ~ 10^3 servers, please contact us on GitHub.
+      # Ideally, you can upload files to 10000 servers in 50 seconds. (2.0 second jitter + 0.5 second execution time)
+      MAX_PARALLEL_TO_UPLOAD = 500
+      JITTER_DURATION_TO_UPLOAD = 2.0
+
       private
 
       # @see lib/capistrano/net_storage/base.rb
@@ -20,10 +26,11 @@ module Capistrano
           build_ssh_command(host)
         }
 
-        on release_roles :all, in: :groups, limit: c.max_parallels do |host|
+        on release_roles :all, in: :groups, limit: MAX_PARALLEL_TO_UPLOAD do |host|
           if c.upload_files_by_rsync?
             rsh_option = build_rsh_option.call(host)
             run_locally do
+              sleep Random.rand(JITTER_DURATION_TO_UPLOAD)
               execute :rsync, "-az --rsh='#{rsh_option}' #{files.join(' ')} #{host.hostname}:#{dest_dir}"
             end
           else
