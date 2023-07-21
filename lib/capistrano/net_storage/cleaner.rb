738b8a66fef039b76e85395a6c7766b09fb95cbf
Takumasa Ochi <aeroastro007@gmail.com>
2023-07-21 11:01:59 +0900

    Fix instance_eval issues by accessing SSHKit::Backend.current

--- lib/capistrano/net_storage/cleaner.rb
+++ lib/capistrano/net_storage/cleaner.rb
@@ -14,7 +14,7 @@ module Capistrano
         config = Capistrano::NetStorage.config
 
         run_locally do
-          clean_targets(config.local_releases_path, /^\d{14}$/)
+          Capistrano::NetStorage::Cleaner.clean_targets(config.local_releases_path, /^\d{14}$/)
         end
       end
 
@@ -25,7 +25,7 @@ module Capistrano
         config = Capistrano::NetStorage.config
 
         run_locally do
-          clean_targets(config.local_archives_path, /^\d{14}\.[^.]+$/) # Do not care file extension
+          Capistrano::NetStorage::Cleaner.clean_targets(config.local_archives_path, /^\d{14}\..+$/) # Do not care file extension
         end
       end
 
@@ -35,32 +35,36 @@ module Capistrano
       def cleanup_archives
         config = Capistrano::NetStorage.config
 
-        on release_roles(:all), in: :groups, limit: config.max_parallels do |host|
-          clean_targets(config.archives_path, /^\d{14}\.[^.]+$/) # Do not care file extension
+        on release_roles(:all), in: :groups, limit: config.max_parallels do
+          Capistrano::NetStorage::Cleaner.clean_targets(config.archives_path, /^\d{14}\..+$/) # Do not care file extension
         end
       end
 
       private
 
-      def clean_targets(target_parent_path, target_regexp)
-        files_or_directories = capture(:ls, '-x', target_parent_path).split
-        targets, invalid = files_or_directories.partition { |e| target_regexp =~ e } # Do not care file extension
+      def self.clean_targets(target_parent_path, target_regexp)
+        files_or_directories = backend.capture(:ls, '-x', target_parent_path).split
+        targets, invalid = files_or_directories.partition { |e| target_regexp =~ e }
 
         if invalid.any?
-          warn "Invalid targets in #{target_parent_path} for #{target_regexp.inspect} on #{host}:\n#{invalid.join("\n")}"
+          backend.warn "Invalid targets in #{target_parent_path} on #{backend.host}: #{invalid.inspect}"
         end
 
         if targets.count > fetch(:keep_releases)
-          info "Keeping #{fetch(:keep_releases)} of #{targets.count} in #{target_parent_path} on #{host}"
+          backend.info "Keeping #{fetch(:keep_releases)} of #{targets.count} in #{target_parent_path} on #{backend.host}"
           old_targets = (targets - targets.last(fetch(:keep_releases))).map do |target|
             target_parent_path.join(target).to_s
           end
           old_targets.each_slice(100) do |old_targets_batch|
-            execute :rm, '-rf', *old_targets_batch
+            backend.execute :rm, '-rf', *old_targets_batch
           end
         else
-          info "No old targets (keeping newest #{fetch(:keep_releases)}) in #{target_parent_path} on #{host}"
+          backend.info "No old targets (keeping newest #{fetch(:keep_releases)}) in #{target_parent_path} on #{backend.host}"
         end
+      end
+
+      def self.backend
+        SSHKit::Backend.current
       end
     end
   end
