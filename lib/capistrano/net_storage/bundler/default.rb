1ce9d313e1285ad79bc454201d0cda1e2774f4c5
Takumasa Ochi <aeroastro007@gmail.com>
2023-08-07 13:24:04 +0900

    Allow Capistrano::NetStorage to deploy different Ruby versions

--- lib/capistrano/net_storage/bundler/default.rb
+++ lib/capistrano/net_storage/bundler/default.rb
@@ -20,23 +20,31 @@ module Capistrano
           run_locally do
             within config.local_release_app_path do
               ::Bundler.with_unbundled_env do
-                install_path = Pathname.new('vendor/bundle') # must be a relative path for portability between local and remote
-                execute :mkdir, '-p', install_path
+                with rbenv_version: nil, rbenv_dir: nil do
+                  bundle = if test :rbenv, 'version'
+                    %w[rbenv exec bundle]
+                  else
+                    %w[bundle]
+                  end
 
-                # Sync installed gems from shared directory to speed up installation
-                execute :rsync, '-a', '--delete', "#{config.local_bundle_path}/", install_path
+                  install_path = Pathname.new('vendor/bundle') # must be a relative path for portability between local and remote
+                  execute :mkdir, '-p', install_path
 
-                # Always set config
-                execute :bundle, 'config', 'set', '--local', 'deployment', 'true'
-                execute :bundle, 'config', 'set', '--local', 'path', install_path
-                execute :bundle, 'config', 'set', '--local', 'without', 'development test'
-                execute :bundle, 'config', 'set', '--local', 'disable_shared_gems', 'true'
+                  # Sync installed gems from shared directory to speed up installation
+                  execute :rsync, '-a', '--delete', "#{config.local_bundle_path}/", install_path
 
-                execute :bundle, 'install', '--quiet'
-                execute :bundle, 'clean'
+                  # Always set config
+                  execute *bundle, 'config', 'set', '--local', 'deployment', 'true'
+                  execute *bundle, 'config', 'set', '--local', 'path', install_path
+                  execute *bundle, 'config', 'set', '--local', 'without', 'development test'
+                  execute *bundle, 'config', 'set', '--local', 'disable_shared_gems', 'true'
 
-                # Sync back to shared directory for the next release
-                execute :rsync, '-a', '--delete', "#{install_path}/", config.local_bundle_path
+                  execute *bundle, 'install', '--quiet'
+                  execute *bundle, 'clean'
+
+                  # Sync back to shared directory for the next release
+                  execute :rsync, '-a', '--delete', "#{install_path}/", config.local_bundle_path
+                end
               end
             end
           end
